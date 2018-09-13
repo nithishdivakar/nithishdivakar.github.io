4e1dd7cd66d1f38cd8a6c0ec06f572076120c9b1
ND <ndivakar@cogknit.com>
2018-09-13 15:32:53 +0530

    moving articles to its own repo

--- articles/softmax_classifier/build.sh
+++
@@ -1,5 +0,0 @@
-#pdflatex -shell-escape figure.tex
-#pdflatex -shell-escape table.tex
-python3 ../convert.py softmax_classifier.tex /home/nd/NOTWORK/website/_drafts/softmax-classifier-from-scratch.md
-latexmk -pdf softmax_classifier.tex
-latexmk -c
